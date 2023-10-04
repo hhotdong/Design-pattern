893e15f77ab6fe6c4813a6296966fb4a5403a4f0
leedongju <hhotdong@gmail.com>
2023-10-05 07:51:55 +0900

    Initial commit

---
+++ FactoryPatternExample1.cs
@@ -0,0 +1,78 @@
+// Credit: https://ansohxxn.github.io/design%20pattern/chapter4/
+using UnityEngine;
+
+namespace FactoryPattern.Example.SimpleFactory
+{
+    public abstract class Beverage
+    {
+        public Beverage()
+        {
+            Debug.Log("Construct Beverage");
+        }
+
+        public abstract void Drink();
+    }
+
+    public class Coffee : Beverage
+    {
+        public Coffee()
+        {
+            Debug.Log("Construct Coffee");
+        }
+
+        public override void Drink()
+        {
+            Debug.Log("Drink Coffee");
+        }
+    }
+
+    public class Tea : Beverage
+    {
+        public Tea()
+        {
+            Debug.Log("Construct Tea");
+        }
+
+        public override void Drink()
+        {
+            Debug.Log("Drink Tea");
+        }
+    }
+
+    public class BeverageFactory
+    {
+        public static Beverage Create(string name)
+        {
+            switch (name)
+            {
+                default:
+                    return null;
+
+                case "Coffee":
+                    return new Coffee();
+
+                case "Tea":
+                    return new Tea();
+            }
+        }
+    }
+
+    public class FactoryPatternExample1 : MonoBehaviour
+    {
+        private void Start()
+        {
+            Beverage b1 = BeverageFactory.Create("Coffee");
+            Beverage b2 = BeverageFactory.Create("Tea");
+            b1.Drink();
+            b2.Drink();
+
+            // Output:
+            // Construct Beverage
+            // Construct Coffee
+            // Construct Beverage
+            // Construct Tea
+            // Drink Coffee
+            // Drink Tea
+        }
+    }
+}
---
+++ FactoryPatternExample2.cs
@@ -0,0 +1,95 @@
+// Credit: https://github.com/QianMo/Unity-Design-Pattern/blob/master/Assets/Creational%20Patterns/Factory%20Method%20Pattern/Example1/FactoryMethodPatternExample1.cs
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FactoryPattern.Example.FactoryMethod
+{
+    /// <summary>
+    /// The 'Product' abstract class
+    /// </summary>
+    public abstract class Ingredient { }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    public class Oil         : Ingredient { }
+    public class Garlic      : Ingredient { }
+    public class Kimchi      : Ingredient { }
+    public class SoySauce    : Ingredient { }
+    public class Salt        : Ingredient { }
+    public class Rice        : Ingredient { }
+    public class Peperoncino : Ingredient { }
+    public class Parsely     : Ingredient { }
+    public class PastaNoodle : Ingredient { }
+
+    /// <summary>
+    /// The 'Creator' abstract class
+    /// </summary>
+    public abstract class Recipe
+    {
+        public List<Ingredient> Ingredients => ingredients;
+
+        protected List<Ingredient> ingredients = new List<Ingredient>();
+
+        // Constructor calls abstract Factory method
+        public Recipe()
+        {
+            this.MakeIngredients();
+        }
+
+        // Factory Method
+        public abstract void MakeIngredients();
+    }
+
+    /// <summary>
+    /// A 'ConcreteCreator' class
+    /// </summary>
+    public class KimchiFriedRice : Recipe
+    {
+        // Factory Method implementation
+        public override void MakeIngredients()
+        {
+            ingredients.Add(new Oil());
+            ingredients.Add(new Garlic());
+            ingredients.Add(new Kimchi());
+            ingredients.Add(new SoySauce());
+            ingredients.Add(new Salt());
+            ingredients.Add(new Rice());
+        }
+    }
+
+    public class Spaghetti : Recipe
+    {
+        // Factory Method implementation
+        public override void MakeIngredients()
+        {
+            ingredients.Add(new Oil());
+            ingredients.Add(new Garlic());
+            ingredients.Add(new Peperoncino());
+            ingredients.Add(new PastaNoodle());
+            ingredients.Add(new Parsely());
+        }
+    }
+
+    public class FactoryPatternExample2 : MonoBehaviour
+    {
+        private void Start()
+        {
+            // Note: constructors call Factory Method
+            Recipe[] recipes = new Recipe[2];
+
+            recipes[0] = new KimchiFriedRice();
+            recipes[1] = new Spaghetti();
+
+            // Display recipe ingredients
+            foreach (Recipe recipe in recipes)
+            {
+                Debug.Log("\n" + recipe.GetType().Name + "--");
+                foreach (Ingredient ingredient in recipe.Ingredients)
+                {
+                    Debug.Log(" " + ingredient.GetType().Name);
+                }
+            }
+        }
+    }
+}
